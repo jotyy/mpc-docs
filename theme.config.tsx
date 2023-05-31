6e10144e348d960a106d5599a53f30f3caca2d4b
jotyy <jotyy318@gmail.com>
2023-05-31 23:22:24 +0800

    feat: release part one

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>MPC 文档</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/jotyy/mpc-doc",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/jotyy/mpc-doc",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "MPC 文档 © 2023 by jotyy",
   },
-}
+};
 
-export default config
+export default config;
